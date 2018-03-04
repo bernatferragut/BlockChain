4322c1e2349f2b1e5401d3959003c11b625d0952
elbernat <elbernat@gmail.com>
2018-03-04 16:23:52 -0600

    BblockChain 1
    BblockChain - test1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# BlockChain
+# WEBPACK INSTALLATION
+1. npm init
+2. npm i -D webpack
+3. npm webpack version
+4. webpack view setting => hide nodemodules and .vscode true
+6. package.json
+"scripts": {
+    "dev": "webpack -d --watch"
+  },
+7. webpack.config.js
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename : './dist/app.bundle.js'
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,72 @@
+// var requireDirectory = require('require-directory');
+// module.exports = requireDirectory(module);
+
+// importin SHA256 Cypher Library
+
+const SHA256 = require('crypto-js/sha256');
+
+// BLOCK  CREATION
+class Block {
+    constructor(index, timeStamp, data, previousHash='') {
+        this.index = index;
+        this.timeStamp = timeStamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.timeStamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+// CHAIN CREATION
+class BlockChain {
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "03/03/2018", 'Genesis Block', '0');
+    }
+
+    getLatestBlock() {
+        return 'The latest Block created is: ' + this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i=1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+// WORKING TEST
+
+let bernieCoin = new BlockChain();
+bernieCoin.addBlock(new Block(1, '03/03/2018', { amount: 4 }));
+bernieCoin.addBlock(new Block(2, '04/03/2018', { amount: 9 }));
+
+// Hacking the data
+// bernieCoin.chain[1].data = {amount: 10};
+// Hack the hash
+// bernieCoin.chain[1].hash = bernieCoin.calculateHash();
+
+console.log(JSON.stringify(bernieCoin, null, 2));
+// console.log('is bernieCoin valid? => ' + isChainValid());
+
+
