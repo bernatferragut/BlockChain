cbbf9408bdb13b28242ef6ad7eda7eb8abdcff25
elbernat <elbernat@gmail.com>
2018-03-24 16:54:40 -0400

    Adding CSS Blocks

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 import { Block, BlockChain } from './blockchain1';
 
-// WORKING TEST
+// BLOCKCHAIN CREATION
 let bernieCoin = new BlockChain();
 bernieCoin.addBlock(new Block(1, '03/03/2018', { amount: 3 }));
 bernieCoin.addBlock(new Block(2, '04/03/2018', { amount: 6 }));
 bernieCoin.addBlock(new Block(3, '04/03/2018', { amount: 9 }));
 
+
+// DIV GRAPHIC BLOCK CREATION
+function createDivBlock() {
+    // create new div element
+    let block = document.createElement('div');
+    let dot = document.createElement('div');
+    // add an id 
+    block.setAttribute('id', 'blocks-design');
+    block.setAttribute('class', 'flex-container');
+    dot.setAttribute('id', 'dot-design');
+    dot.setAttribute('class', 'flex-container');
+
+    // text block content
+    let blockContent = document.createTextNode('GENESIS');
+    // add text to the div
+    block.appendChild(blockContent);
+    // add the new Element to the DOM
+    let currentDiv = document.getElementById('blocks');
+    currentDiv.appendChild(block);
+    currentDiv.appendChild(dot);
+    // send a message
+    console.log('new block has been added');
+}
 
 // SELECTIONS
 let blockchainData = JSON.stringify(bernieCoin, null, 2);
@@ -15,8 +38,10 @@ let data =  document.getElementById('data');
 
 // ACTIONS
 button1.addEventListener('click', function() {
-    data.innerHTML = JSON.stringify(bernieCoin, null, 2);
-    // console.log(JSON.stringify(bernieCoin, null, 2));
+    // data.innerHTML = JSON.stringify(bernieCoin, null, 2);
+    // console.log('The Button has been clicked!');
+    createDivBlock();
+
 });
 
 
